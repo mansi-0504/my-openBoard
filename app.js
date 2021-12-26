a0721fece1bea8c42907707028744ebbb108c174
Mansi Shevgaonkar <mansiscmc19@svvv.edu.in>
2021-12-26 16:09:14 +0530

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
  const app = express();//initialize the application and server ready
 
  app.use(express.static("public"));
- let port = 5000;
+ let port = process.env.PORT || 5000;
  let server = app.listen(port, ()=>{
      console.log("listening to port " + port);
  })
